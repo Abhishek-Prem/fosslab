a76198f16308d578ba4f7660ff9daa20b1ebfc36
Abhishek-Prem <abhishekpnambissan@gmail.com>
2018-04-12 09:02:11 +0530

    KernelCompilation

---
+++ KernelCompilation/kernel.sh
@@ -0,0 +1,11 @@
+wget https://cdn.kernel.org/pub/linux/kernel/v4.x/linux-4.16.1.tar.xz
+
+tar -xvJf linux-4.16.1.tar.xz
+
+cd linux-4.16.1
+
+sudo apt-get install fakeroot build-essential ncurses-dev xz-utils libssl-dev
+
+sudo make menuconfig
+
+sudo make -j 4 && sudo make modules_install -j 4 && sudo make install -j 4
